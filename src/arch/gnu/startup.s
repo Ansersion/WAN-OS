e2d7ecdab5596a83932b5fe54ec2c3a8bb63910e
ansersion <ansersion@gmail.com>
2016-08-07 03:38:34 -0700

    Modify sct.ld

--- src/arch/gnu/startup.s
+++ src/arch/gnu/startup.s
@@ -1,5 +1,5 @@
 .EQU 	_MSP_TOP,		0x20008000              
-.EQU 	_RAM_ADDR, 		0x20000000
+@ .EQU 	_RAM_ADDR, 		0x20000000
 
 .text
 .global 	_start, _irq_vectors
@@ -10,10 +10,18 @@ _start:
 				@ Stack has been destroyed, so
 				@ invoking IRQ_LOCK is dangerous
 				CPSID	I 	@ IRQ_LOCK
-				ldr 	r0, =_MSP_TOP
-				msr		msp,	r0
-				mov 	r0, #0
-				msr 	control, r0
+
+				ldr 	r0, 	=_MSP_TOP
+				msr 	msp, 	r0
+
+				mov 	r0, 	#0
+				msr 	control, 	r0
+
+				mov 	r0, 	#0x01000000
+				msr 	psr,  	r0
+
+				mov 	r0, 	#0x0
+				msr 	psp,  	r0
 				
 				mov 	r0, #0
 				mov 	r1, #0
@@ -28,6 +36,7 @@ _start:
 				mov 	r10, #0
 				mov 	r11, #0
 				mov 	r12, #0
+				mov 	lr, #0xfffffff9
 
 				bl 		main
 
@@ -39,20 +48,35 @@ halt_loop:
 
 _irq_vectors:
 	.word _MSP_TOP
-	.word _RAM_ADDR+_start+1 			@ '+1' is used to avoid UsageFault
-	.word _RAM_ADDR+IRQ_Nmi 	
-	.word _RAM_ADDR+IRQ_HardFault 
-	.word _RAM_ADDR+IRQ_MemManage
-	.word _RAM_ADDR+IRQ_BusFault 	
-	.word _RAM_ADDR+IRQ_UsageFault 
-	.word _RAM_ADDR+IRQ_NULL_7 	
-	.word _RAM_ADDR+IRQ_NULL_8
-	.word _RAM_ADDR+IRQ_NULL_9 	
-	.word _RAM_ADDR+IRQ_NULL_10 	
-	.word _RAM_ADDR+IRQ_SVC 	
-	.word _RAM_ADDR+IRQ_DebugMon
-	.word _RAM_ADDR+IRQ_NULL_13 	
-	.word _RAM_ADDR+IRQ_PendSV 	
-	.word _RAM_ADDR+IRQ_SysTick
+	.word _start+1 			@ '+1' is used to avoid UsageFault
+	.word IRQ_Nmi 	
+	.word IRQ_HardFault 
+	.word IRQ_MemManage
+	.word IRQ_BusFault 	
+	.word IRQ_UsageFault 
+	.word IRQ_NULL_7 	
+	.word IRQ_NULL_8
+	.word IRQ_NULL_9 	
+	.word IRQ_NULL_10 	
+	.word IRQ_SVC 	
+	.word IRQ_DebugMon
+	.word IRQ_NULL_13 	
+	.word IRQ_PendSV 	
+	.word IRQ_SysTick
+	@ .word _RAM_ADDR+_start+1 			@ '+1' is used to avoid UsageFault
+	@ .word _RAM_ADDR+IRQ_Nmi 	
+	@ .word _RAM_ADDR+IRQ_HardFault 
+	@ .word _RAM_ADDR+IRQ_MemManage
+	@ .word _RAM_ADDR+IRQ_BusFault 	
+	@ .word _RAM_ADDR+IRQ_UsageFault 
+	@ .word _RAM_ADDR+IRQ_NULL_7 	
+	@ .word _RAM_ADDR+IRQ_NULL_8
+	@ .word _RAM_ADDR+IRQ_NULL_9 	
+	@ .word _RAM_ADDR+IRQ_NULL_10 	
+	@ .word _RAM_ADDR+IRQ_SVC 	
+	@ .word _RAM_ADDR+IRQ_DebugMon
+	@ .word _RAM_ADDR+IRQ_NULL_13 	
+	@ .word _RAM_ADDR+IRQ_PendSV 	
+	@ .word _RAM_ADDR+IRQ_SysTick
 
 	.end
